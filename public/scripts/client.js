f98ae9d01ad278cb72e18e5a238c08463b3faaf9
Gordonamaka <amobimak@gmail.com>
2021-10-07 23:21:23 +0000

    same as per previous commit with dead code & comments removed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,6 @@
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 $(document).ready(() => {
   //createTweetElement function
