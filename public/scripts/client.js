6e47f08a0ed7d2dac8ac43050b2d53d1a9dc93d1
Gordonamaka <amobimak@gmail.com>
2021-10-07 23:18:43 +0000

    if this commit is used, expect full app functionality with tweets and responsive design

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,21 +4,23 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 $(document).ready(() => {
+  //createTweetElement function
   const createTweetElement = function (data) {
+    // Template string for tweet structure
     let $tweet = `
  <article>
     <header>
       <div class="picId">
-        <img src= "${data.user.avatars}">
-        <p>${data.user.name}</p>
+        <img src= "${escape(data.user.avatars)}">
+        <p>${escape(data.user.name)}</p>
       </div>
-        <p>${data.user.handle}</p>
+        <p>${escape(data.user.handle)}</p>
     </header>  
     <content>
-        <p>${data.content.text}</p>
+        <p>${escape(data.content.text)}</p>
     </content>
     <footer>
-      <p>${timeago.format(data.created_at)} </p>
+      <p>${timeago.format(escape(data.created_at))} </p>
       <div>
         <i class="fas fa-flag"></i>
         <i class="fas fa-retweet"></i>
@@ -29,7 +31,16 @@ $(document).ready(() => {
     return $tweet;
   };
 
-  // render tweets function
+  // Escape function to disable malicious XSS attacks
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+
+
+  // render tweets function to loop and prepend tweets in descending order
   const renderTweets = function (tweets) {
 
     for (let ele of tweets) {
@@ -39,40 +50,43 @@ $(document).ready(() => {
 
   };
 
-  // form submission prevent request
+  // form submission prevent request.
   $(function () {
-    // constants
-    const $form = $('#more-tweets');
-    const $lengthError = $('#length-error');
-    const $emptyError = $('#empty-error');
-    const $tweetText = $('#tweet-text');
-    const $countdown = $('.counter');
+    // constants, altered to traverse the DOM for good practice.
+    const $form = $('section form');
+    const $lengthError = $('section p#length-error');
+    const $emptyError = $('section p#empty-error');
+    const $tweetText = $('section form textarea');
+    const $countdown = $('section form div output');
 
+    //form submission prevent request.
     $form.on('submit', function (event) {
       event.preventDefault();
       console.log('Button clicked, preventing default action of the form & performing ajax call...');
+      //Variables to serialize form input & specify the URL used. 
       const data = $form.serialize();
       const tweetUrl = "http://localhost:8080/tweets";
-      
-      //Add in more .then's to valid slide down would be a catch to a jquery selector for an error
+
+      //conditonal to check for jquery selector error's, tests for empty space & null.
       if ($tweetText.val().length > 140) {
         return $lengthError.slideDown();
-      } else if ($tweetText.val() === ""||$tweetText.val() === null) { // test for empty space & null
+      } else if ($tweetText.val().trim() === "" || $tweetText.val() === null) {
         return $emptyError.slideDown();
       }
 
+      // Ajax request to POST data to page w/ slide up effects after error(s) are corrected & refresh the character count & textarea.
       $.ajax({ data: data, url: tweetUrl, method: 'POST' })
         .then(function () {
           $lengthError.slideUp();
           $emptyError.slideUp();
           $tweetText.val("");
-          $countdown.html("140");
+          $countdown.text("140");
           return loadTweets();
         });
     });
   });
 
-  // form submission to get the new information
+  // load tweets form submission function to get the info posted, also emptys the tweet body upon server restart.
   const loadTweets = function () {
     const tweetUrl = "http://localhost:8080/tweets";
     $.ajax({ url: tweetUrl, method: 'GET' })
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,12 @@
 // solely responsible for character count
 $(document).ready(() => {
-
+  // traverses the DOM to the textarea, then creates a variable to update the character count & change it to red if it goes beyond 140 characters and reverses it as well.
   $('.new-tweet form textarea').on('keyup', function () {
     const counterUpdate = $(this).parent('form').children('.button-counter').children('output');
     const countdown = 140 - $(this).val().length;
     counterUpdate.text(countdown);
 
+    // Conditional statement to turn counter red if < 0.
     if (countdown < 0) {
       counterUpdate.addClass('neg');
     } else {
@@ -14,7 +15,3 @@ $(document).ready(() => {
   });
 });
 
-
-// document.addEventListener("DOMContentLoaded", function(event) { 
-  //do work
-// }); This is the .ready function without jQuery
