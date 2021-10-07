2e0bfc1b73a237588fade8eeb99eafd4c037dd68
Gordonamaka <amobimak@gmail.com>
2021-10-07 16:00:43 +0000

    render/load tweets functionality works/displaying validation errors with jQuery works also with this commit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,26 +3,88 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
+$(document).ready(() => {
+  const createTweetElement = function (data) {
+    let $tweet = `
+ <article>
+    <header>
+      <div class="picId">
+        <img src= "${data.user.avatars}">
+        <p>${data.user.name}</p>
+      </div>
+        <p>${data.user.handle}</p>
+    </header>  
+    <content>
+        <p>${data.content.text}</p>
+    </content>
+    <footer>
+      <p>${timeago.format(data.created_at)} </p>
+      <div>
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+  </article>`;
+    return $tweet;
+  };
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+  // render tweets function
+  const renderTweets = function (tweets) {
+
+    for (let ele of tweets) {
+      const loop = createTweetElement(ele);
+      $('.all-tweets').prepend(loop);
+    };
+
+  };
+
+  // form submission prevent request
+  $(function () {
+    // constants
+    const $form = $('#more-tweets');
+    const $lengthError = $('#length-error');
+    const $emptyError = $('#empty-error');
+    const $tweetText = $('#tweet-text');
+    const $countdown = $('.counter');
+
+    $form.on('submit', function (event) {
+      event.preventDefault();
+      console.log('Button clicked, preventing default action of the form & performing ajax call...');
+      const data = $form.serialize();
+      const tweetUrl = "http://localhost:8080/tweets";
+      
+      //Add in more .then's to valid slide down would be a catch to a jquery selector for an error
+      if ($tweetText.val().length > 140) {
+        return $lengthError.slideDown();
+      } else if ($tweetText.val() === ""||$tweetText.val() === null) { // test for empty space & null
+        return $emptyError.slideDown();
+      }
+
+      $.ajax({ data: data, url: tweetUrl, method: 'POST' })
+        .then(function () {
+          $lengthError.slideUp();
+          $emptyError.slideUp();
+          $tweetText.val("");
+          $countdown.html("140");
+          return loadTweets();
+        });
+    });
+  });
+
+  // form submission to get the new information
+  const loadTweets = function () {
+    const tweetUrl = "http://localhost:8080/tweets";
+    $.ajax({ url: tweetUrl, method: 'GET' })
+      .then(function (response) {
+        $(".all-tweets").empty();
+        return renderTweets(response);
+      })
+  };
+
+  //When you reload the page.
+  loadTweets();
+});
 
 
-const $tweet = createTweetElement(tweetData);
-
-console.log($tweet);
-
-$('#tweets-container').append($tweet); // this may be the content tab, not container.
-
-
-// <meta name="viewport" content="width=device-width, initial-scale=1"> Necessary with all responsive design
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,9 @@
 // solely responsible for character count
 $(document).ready(() => {
 
-  $('#tweet-text').on('keyup', function () {
-    const counterUpdate = $('#counter')
-    const countdown = 140 - $(this).val().length
+  $('.new-tweet form textarea').on('keyup', function () {
+    const counterUpdate = $(this).parent('form').children('.button-counter').children('output');
+    const countdown = 140 - $(this).val().length;
     counterUpdate.text(countdown);
 
     if (countdown < 0) {
@@ -11,11 +11,10 @@ $(document).ready(() => {
     } else {
       counterUpdate.removeClass('neg');
     }
-    // console.log($counterUpdate.text());
   });
 });
 
-//.parent().siblings('.output').children('.button-counter').children('.counter')
+
 // document.addEventListener("DOMContentLoaded", function(event) { 
   //do work
 // }); This is the .ready function without jQuery
